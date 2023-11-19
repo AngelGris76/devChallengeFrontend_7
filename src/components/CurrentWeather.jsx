0a6d17bf0fb2988166d14915bc3b7fb35d4d4a4d
Sebastian Smuraglia <sebastian.smuraglia@gmail.com>
2023-11-19 19:40:32 -0300

    add location pin on currentweather and change style

--- src/components/CurrentWeather.jsx
+++ src/components/CurrentWeather.jsx
@@ -1,5 +1,6 @@
 import { weatherCodes } from '../values/weatherCodes';
 import './CurrentWeather.css';
+import { LocationPin } from './icons/LocationPin';
 
 export const CurrentWeather = ({ todayWeather, city }) => {
 	const today = new Date(todayWeather.time).toUTCString().slice(0, 11);
@@ -9,14 +10,26 @@ export const CurrentWeather = ({ todayWeather, city }) => {
 
 	return (
 		<article className='currentWeather'>
-			<p>{city}</p>
-			<p>{`Today - ${today}`}</p>
-			<p>{weatherDescription}</p>
-			<p>
-				<span>{todayWeather.temperature_2m}</span>
-				<span>°c</span>
+			<p className='currentWeather__city'>
+				<span>
+					<LocationPin width='1.25rem' height='1.25rem' />
+				</span>
+				<span>{city}</span>
 			</p>
-			<Icon width='100' height='100' />
+			<p className='currentWeather__date'>
+				{/*	{`Today - ${today}`} */}
+				<span>Today</span>
+				<span>-</span>
+				<span>{today}</span>
+			</p>
+			<p className='currentWeather__description'>{weatherDescription}</p>
+			<p>
+				<span className='currentWeather__temperatureValue'>
+					{todayWeather.temperature_2m}
+				</span>
+				<span className='currentWeather__temperatureUnit'>°C</span>
+			</p>
+			<Icon width='150px' height='174px' />
 		</article>
 	);
 };
