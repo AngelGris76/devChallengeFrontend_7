3510ff49792addcc2de7502ad30b8fcfc0d30138
Sebastian Smuraglia <sebastian.smuraglia@gmail.com>
2023-11-26 18:55:36 -0300

    add css transition on sercha section

--- src/components/SearchSection.jsx
+++ src/components/SearchSection.jsx
@@ -1,10 +1,68 @@
+import { useEffect, useRef, useState } from 'react';
 import { useCities } from '../hooks/useCities';
 import { SearchBar } from './SearchBar';
 import { Spinner } from './Spinner';
 import './SearchSection.css';
 
+// -------
+
+const TRANSITION_STATUS = {
+	init: 'transitionInit',
+	finished: 'transitionFinished',
+	close: 'windowClose',
+	selectedCity: 'changeCity',
+};
+
+// ------
+
 export const SearchSection = ({ handleClose, geoLocSetter }) => {
 	const { cities, searching, refreshCities } = useCities();
+	// ----------
+
+	const [transition, setTransition] = useState(TRANSITION_STATUS.init);
+	const [geoLocData, setGeoLocData] = useState({});
+	const referenced = useRef();
+
+	useEffect(() => {
+		const styleTransition = (ev) => {
+			if (
+				ev.target === referenced.current &&
+				transition === TRANSITION_STATUS.close
+			)
+				handleClose();
+			else {
+				if (
+					ev.target === referenced.current &&
+					transition === TRANSITION_STATUS.selectedCity
+				)
+					geoLocSetter(geoLocData);
+			}
+		};
+
+		let timeOut;
+
+		document.addEventListener('transitionend', styleTransition);
+
+		if (transition === TRANSITION_STATUS.init) {
+			timeOut = setTimeout(setTransition(TRANSITION_STATUS.finished), 10);
+		}
+
+		if (transition === TRANSITION_STATUS.finished)
+			referenced.current.classList.add('moved');
+
+		if (transition === TRANSITION_STATUS.close)
+			referenced.current.classList.remove('moved');
+
+		if (transition === TRANSITION_STATUS.selectedCity)
+			referenced.current.classList.remove('moved');
+
+		return () => {
+			clearTimeout(timeOut);
+			document.removeEventListener('transitionend', styleTransition);
+		};
+	}, [transition, handleClose, geoLocSetter, geoLocData]);
+
+	// -----------
 
 	const handleSearchCities = (ev) => {
 		ev.preventDefault();
@@ -15,9 +73,12 @@ export const SearchSection = ({ handleClose, geoLocSetter }) => {
 	const handleCityButton = ({ cityID }) => {
 		const selectedCity = cities.filter((city) => city.id === cityID)[0];
 		const { latitude, longitude, timezone, name } = selectedCity;
-		const geoLocData = { name, latitude, longitude, timezone };
+		// const geoLocData = { name, latitude, longitude, timezone };
 
-		geoLocSetter(geoLocData);
+		setGeoLocData({ name, latitude, longitude, timezone });
+
+		setTransition(TRANSITION_STATUS.selectedCity);
+		// geoLocSetter(geoLocData);
 	};
 
 	const buttonsToRender = cities.map((city) => (
@@ -37,11 +98,11 @@ export const SearchSection = ({ handleClose, geoLocSetter }) => {
 	));
 
 	return (
-		<div className='searchDialog'>
+		<div className='searchDialog' ref={referenced}>
 			<button
 				className='searchDialog__closeButton'
 				type='button'
-				onClick={handleClose}
+				onClick={() => setTransition(TRANSITION_STATUS.close)}
 			>
 				X
 			</button>
