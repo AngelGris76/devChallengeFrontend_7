86ecc49328fe57b152539137be68c84d4e181061
Sebastian Smuraglia <sebastian.smuraglia@gmail.com>
2023-11-26 10:58:30 -0300

    modified searchbar and add searchbar style file

--- src/components/SearchBar.jsx
+++ src/components/SearchBar.jsx
@@ -1,11 +1,19 @@
+import { Search } from './icons/Search';
+import './SearchBar.css';
+
 export const SearchBar = ({ onClick }) => {
 	return (
 		<search className='searchBar' role='search'>
-			<input
-				className='searchBar__input'
-				type='text'
-				placeholder='Search location'
-			></input>
+			<label htmlFor='' className='searchBar__inputContainer'>
+				<span className='searchBar__icon'>
+					<Search width={'1.5rem'} height={'1.5rem'} />
+				</span>
+				<input
+					className='searchBar__input'
+					type='text'
+					placeholder='Search location'
+				></input>
+			</label>
 			<button className='searchBar__button' type='button' onClick={onClick}>
 				Search
 			</button>
